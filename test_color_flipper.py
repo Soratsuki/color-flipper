4ee48fd3baec1097b334511d4955c968c0e45e8a
soratsuki <obriansan@outlook.com>
2021-06-29 16:23:10 -0500

    Set the absolute path for the index.html file so that selenium would find the path to the file

--- test_color_flipper.py
+++ test_color_flipper.py
@@ -1,13 +1,24 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import os
 
 class Color_Flipper_Tests(unittest.TestCase):
+
+    file_path = os.path.abspath("./index.html")
+
     def setUp(self) -> None:
         self.driver = webdriver.Chrome("C:\Program Files (x86)\chromedriver.exe")
-        self.driver.get("index.html")
+        self.driver.get(Color_Flipper_Tests.file_path)
+        time.sleep(5)
 
     def test_title(self):
-        assert self.driver.title == "Color Flipper"
+        assert "Color Flipper" in self.driver.title 
+        
 
     def tearDown(self) -> None:
         self.driver.close()
